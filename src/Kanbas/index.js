b9cc23f2978c3072c77404a4e00169b35a7153ca
Manpingzz <zhao.manp@northeastern.edu>
2023-10-10 12:17:07 -0700

    Finished Lab part

---
+++ src/Kanbas/index.js
@@ -0,0 +1,15 @@
+import Nav from "../Nav";
+
+function Kanbas() {
+    return(
+       <div>
+         <Nav/>
+         <h1>Kanbas</h1>
+       </div>
+    );
+ }
+ export default Kanbas;
+ 
+
+ 
+ 
---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,44 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ES5Functions from "./ES5Functions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import House from "./House";
+import Spread from "./Spread";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import PathParameters from "./PathParameters";
+
+
+
+function JavaScript() {
+    return(
+       <div>
+          <h1>JavaScript</h1>
+          <VariablesAndConstants />
+          <VariableTypes />
+          <BooleanVariables />
+          <IfElse />
+          <TernaryOperator />
+          <ES5Functions />
+          <ArrowFunctions />
+          <ImpliedReturn />
+          <FunctionParenthesisAndParameters />
+          <WorkingWithArrays />
+          <House />
+          <Spread />
+          <Destructing />
+          <FunctionDestructing />
+          <PathParameters />
+          
+       </div>
+    );
+ }
+ export default JavaScript;
+ 
+ 
