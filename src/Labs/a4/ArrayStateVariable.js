863ce0e073ab0a53ee5a39efc0a79ff7255e2360
Manpingzz <zhao.manp@northeastern.edu>
2023-10-20 23:51:15 -0700

    a4-Labs part(2.3.8)

---
+++ src/Labs/a4/ArrayStateVariable.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function ArrayStateVariable() {
+    const [array, setArray] = useState([1, 2, 3, 4, 5]);
+    const addElement = () => {
+        setArray([...array, Math.floor(Math.random() * 100)]);
+    };
+    const deleteElement = (index) => {
+        setArray(array.filter((item, i) => i !== index));
+    };
+
+    return (
+        <div>
+            <h2>Array State Variable</h2>
+            <button className="btn btn-success"
+                onClick={addElement}>Add Element</button>
+            <table style={{ width: '15%', marginTop: '15px', borderCollapse: 'collapse' }}>                    
+            <tbody>
+                    {array.map((item, index) => (
+                        <tr key={index}>
+                            <td style={{ padding: '5px', border: '1px solid #ddd', display: 'flex', justifyContent: 'space-between' }}>
+                                <span>{item}</span>
+                                <button className="btn btn-danger"
+                                    onClick={() => deleteElement(index)}>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+export default ArrayStateVariable;
+
