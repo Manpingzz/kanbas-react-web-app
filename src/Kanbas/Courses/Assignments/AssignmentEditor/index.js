4e844d6af2fa81e13b6dc040c06c8856f60258fe
Manpingzz <zhao.manp@northeastern.edu>
2023-11-12 13:43:51 -0800

    Finished assignment part

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -6,131 +6,147 @@ import {
   addAssignment,
   deleteAssignment,
   updateAssignment,
-  setAssignment,
-
+  setAssignments,
+  selectAssignment,
 } from "../assignmentsReducer";
 import { useDispatch, useSelector } from "react-redux";
-
-
+import axios from "axios";
+import * as client from "../client";
 
 function AssignmentEditor() {
   const { assignmentId, courseId } = useParams();
   const navigate = useNavigate();
-  const allAssignments = useSelector((state) => state.assignmentsReducer.assignments);
+
+  const allAssignments = useSelector(
+    (state) => state.assignmentsReducer.assignments
+  );
+
+  // const assignment = useSelector(
+  //   (state) => state.assignmentsReducer.assignment
+  // );
   const dispatch = useDispatch();
 
   const [assignment, setAssignment] = useState(() => {
     if (assignmentId === "Create") {
       // create new assignment
       return {
-        title: '',
-        description: '',
-        dueDate: '',
-        availableFromDate: '',
-        availableUntilDate: '',
+        title: "",
+        description: "",
+        dueDate: "",
+        availableFromDate: "",
+        availableUntilDate: "",
       };
     } else {
       // edit an assignment
-      return allAssignments.find(assignment => assignment._id === assignmentId);
+      return allAssignments.find(
+        (assignment) => assignment._id === assignmentId
+      );
     }
   });
 
   const handleChange = (field) => (e) => {
-    setAssignment(prev => ({
+    setAssignment((prev) => ({
       ...prev,
-      [field]: e.target.value
+      [field]: e.target.value,
     }));
   };
 
-  const handleSave = () => {
-    if (assignmentId === "Create") {
-      const newAssignmentId = "A" + (allAssignments.length + 101);
-      const newAssignment = {
-        ...assignment,
-        _id: newAssignmentId,
-        course: courseId
-      };
+  const handleAddAssignment = async () => {
+    const newAssignment = await client.createAssignment(courseId, assignment);
+    dispatch(addAssignment(newAssignment));
+  };
 
-      dispatch(addAssignment(newAssignment));
+  const handleUpdateAssignment = async () => {
+    const updatedAssignment = await client.updateAssignment(assignment);
+    dispatch(updateAssignment(updatedAssignment));
+  };
+
+  const handleSave = async () => {
+    if (assignmentId === "Create") {
+      console.log("Creating New Assignment");
+      await handleAddAssignment(assignment); // 等待添加操作完成
     } else {
-      const updatedAssignment = { ...assignment, course: courseId };
-      dispatch(updateAssignment(updatedAssignment));
+      console.log("Updating assignment");
+      await handleUpdateAssignment(assignment); // 等待更新操作完成
     }
 
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
 
-
   return (
     <div>
       <div className="float-end">
-        <i className="fas fa-check-circle" style={{ color: 'green', marginRight: '5px' }}></i>
+        <i
+          className="fas fa-check-circle"
+          style={{ color: "green", marginRight: "5px" }}
+        ></i>
         <span>Published</span>
         <button className="btn btn-light btn-sm ms-2">
-          <i className="fa fa-ellipsis-v" style={{ color: 'black' }}></i>
+          <i className="fa fa-ellipsis-v" style={{ color: "black" }}></i>
         </button>
       </div>
 
-      <div style={{ clear: 'both' }}></div>
+      <div style={{ clear: "both" }}></div>
 
       <hr />
       <div>
         <h6>Assignment Name</h6>
-        <input value={assignment.title}
-          onChange={handleChange('title')}
+        <input
+          value={assignment.title}
+          onChange={handleChange("title")}
           placeholder="New Assignment"
-          className="form-control mb-2" />
+          className="form-control mb-2"
+        />
 
         <textarea
           value={assignment.description}
-          onChange={handleChange('description')}
+          onChange={handleChange("description")}
           placeholder="New Assignment Description"
           className="form-control mb-2"
-          rows="3">
-        </textarea>
+          rows="3"
+        ></textarea>
 
         <div className="row">
-          <div className="col-4">
-          </div>
+          <div className="col-4"></div>
           <div className="col-8">
             <div className="mb-2 d-flex align-items-center">
               <label className="me-2">Points</label>
               <input
                 type="number"
                 value={assignment.points || 100}
-                onChange={handleChange('points')}
-
+                onChange={handleChange("points")}
                 className="form-control"
               />
             </div>
 
-
             <div className="mb-2 d-flex align-items-center">
               <label className="me-2">Assign</label>
               <div className="border p-3 w-100">
-
                 <div className="row mb-2">
                   <div className="col">
-                  <label>Due</label>
+                    <label>Due</label>
                     <div className="date-input-container">
-                      <input type="date"
+                      <input
+                        type="date"
                         value={assignment.dueDate}
-                        onChange={handleChange('dueDate')}
-                        className="form-control" />
+                        onChange={handleChange("dueDate")}
+                        className="form-control"
+                      />
                       <i className="fa fa-calendar"></i>
                     </div>
                   </div>
                 </div>
 
-
                 <div className="row">
                   <div className="col">
                     <label>Available from</label>
                     <div className="date-input-container">
-                      <input type="date"
+                      <input
+                        type="date"
                         value={assignment.availableFromDate}
-                        onChange={handleChange('availableFromDate')}
-                        className="form-control mb-2" />
+                        onChange={handleChange("availableFromDate")}
+                        className="form-control mb-2"
+                      />
                       <i className="fa fa-calendar"></i>
                     </div>
                   </div>
@@ -138,10 +154,12 @@ function AssignmentEditor() {
                   <div className="col relative-position">
                     <label>Until</label>
                     <div className="date-input-container">
-                      <input type="date"
+                      <input
+                        type="date"
                         value={assignment.availableUntilDate}
-                        onChange={handleChange('availableUntilDate')}
-                        className="form-control mb-2" />
+                        onChange={handleChange("availableUntilDate")}
+                        className="form-control mb-2"
+                      />
                       <i className="fa fa-calendar"></i>
                     </div>
                   </div>
@@ -152,7 +170,10 @@ function AssignmentEditor() {
           <hr />
         </div>
         <div className="float-end">
-          <Link to={`/Kanbas/Courses/${courseId}/Assignments`} className="btn btn-light me-2">
+          <Link
+            to={`/Kanbas/Courses/${courseId}/Assignments`}
+            className="btn btn-light me-2"
+          >
             Cancel
           </Link>
           <button onClick={handleSave} className="btn btn-danger">
@@ -160,11 +181,8 @@ function AssignmentEditor() {
           </button>
         </div>
       </div>
-
     </div>
   );
 }
 
-
 export default AssignmentEditor;
-
